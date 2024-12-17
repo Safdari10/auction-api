272ac974609e1c9b29f1bcf604533f06775de847
Zaker Safdari <zakersafdari@gmail.com>
2024-12-17 16:19:04 +1300

    Added CORS support to server configuration

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const searchRoutes = require('./routes/searchRoutes');
@@ -9,7 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+app.use(cors());
 app.use(bodyParser.json());
 
 
