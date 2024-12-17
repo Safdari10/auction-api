bab8c83d965875427d9ff0fa7861f41efb4c2fad
Zaker Safdari <zakersafdari@gmail.com>
2024-12-17 16:59:49 +1300

    Updated database connection import and configured dotenv in mongoDB setup

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const connectDB = require('./src/config/db');
+const connectDB = require('./src/config/mongoDB');
 const searchRoutes = require('./src/routes/searchRoutes');
 
 
