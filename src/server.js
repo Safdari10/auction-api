89d4cac7bdd593250f983113b18caed3266ff1bd
Zaker Safdari <zakersafdari@gmail.com>
2024-12-17 16:14:08 +1300

    Added body-parser dependency and integrated it into the server setup

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const searchRoutes = require('./routes/searchRoutes');
+const bodyParser = require('body-parser');
