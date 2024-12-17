1e066317076a48c5b4de85d009d12707e0df4d84
Zaker Safdari <zakersafdari@gmail.com>
2024-12-17 15:31:56 +1300

    Added and exported MongoDB connection function with error handling

--- src/config/mongoDB.js
+++ src/config/mongoDB.js
@@ -2,3 +2,15 @@ const mongoose = require('mongoose');
 dotenv.config();
 
 const URI = process.env.MONGO_URI;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(URI);
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error("MongDB connection error:" ,error.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
